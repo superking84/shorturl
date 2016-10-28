5aa525ccfb6e8eeeba451840f606551e5c216572
Frank Harvey <frankharvey88@gmail.com>
2016-10-28 20:03:44 +0000

    added some basic database logic to practice

--- server.js
+++ server.js
@@ -5,11 +5,18 @@ var MongoClient = mongodb.MongoClient
 var app = express()
 
 app.get('*', function(req, res){
+    var url = req.url.slice(1)
+    
     MongoClient.connect(process.env.MONGOLAB_URI, function (err, db) {
+      var urls = db.collection('urls');
+      
       if (err) {
         console.log(process.env.MONGOLAB_URI)
         console.log('Unable to connect to the mongoDB server. Error:', err)
       } else {
+        urls.insert({
+            url: url
+        })
         console.log('Connection established to', process.env.MONGOLAB_URI)
     
         // do some work here with the database.
