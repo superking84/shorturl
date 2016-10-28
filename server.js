314062905d5a81a15f18dd583cfa631fc580b555
Frank Harvey <frankharvey88@gmail.com>
2016-10-28 19:31:45 +0000

    added barebones functionality with mongodb

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 var express = require("express")
+var mongodb = require("mongodb")
+var MongoClient = mongodb.MongoClient
+
 var app = express()
 
 app.get('*', function(req, res){
+    MongoClient.connect(process.env.MONGOLAB_URI, function (err, db) {
+      if (err) {
+        console.log(process.env.MONGOLAB_URI)
+        console.log('Unable to connect to the mongoDB server. Error:', err)
+      } else {
+        console.log('Connection established to', process.env.MONGOLAB_URI)
+    
+        // do some work here with the database.
+    
+        //Close connection
+        db.close()
+      }
+    })
     
     res.end("Test")
 })
